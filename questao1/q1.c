ec46af434144c2013389d27d92b80d5a20dd9753
elaynecarvalhosouza <elayne.carvalho.016@ufrn.edu.br>
2021-06-22 23:48:21 -0300

    atualização comit

--- questao1/q1.c
+++ questao1/q1.c
@@ -10,5 +10,7 @@ int main(){
     printf("A expressão 'p == &i' retorna o valor %d\n", (p== &i));
     printf("A expressão '*p - *q' retorna o valor %d\n", (*p - *q));
     printf("A expressão '**&p' retorna o valor %d\n", (**&p));
-    //printf("A expressão 'p == &i' retorna o valor %d\n", (p== &i));
+    printf("A expressão '3 - *p/(*q) + 7' retorna o valor %d\n", (3 - *p/(*q) + 7));
+
+    return 0;
 }
