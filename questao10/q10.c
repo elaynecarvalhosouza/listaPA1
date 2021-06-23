a9b55bb67497c5600719671cf9cfb4fa0d54dcf0
elaynecarvalhosouza <elayne.carvalho.016@ufrn.edu.br>
2021-06-23 11:48:43 -0300

    quest10

---
+++ questao10/q10.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    //para roda o x com o TIPO que preferi descomente;
+
+    
+    char x[4];
+    printf("endereço inicial x= %d \n",x);
+    printf("x = %d \n", x+1); // somou +1 (1*1)
+    printf("x = %d \n", x+2); // somou +2 (2*1)
+    printf("x = %d \n", x+3); // somou +3 (3*1)
+    
+
+    /*
+    int x[4];
+    printf("endereço inicial x= %d \n",x);
+    printf("x = %d \n", x+1); // somou +4 (1*4)
+    printf("x = %d \n", x+2); // somou +8 (2*4)
+    printf("x = %d \n", x+3); // somou +12 (3*4)
+    */
+
+    /*
+    float x[4];
+    printf("endereço inicial x= %d \n",x);
+    printf("x = %d \n", x+1); // somou +4 (1*4)
+    printf("x = %d \n", x+2); // somou +8 (2*4)
+    printf("x = %d \n", x+3); // somou +12 (3*4)
+    */
+
+    /*
+    double x[4];
+    printf("endereço inicial x= %d \n",x);
+    printf("x = %d \n", x+1); // somou +8 (1*8)
+    printf("x = %d \n", x+2); // somou +16 (2*8)
+    printf("x = %d \n", x+3); // somou +24 (3*8)
+    */
+
+
+    //comparando com o que foi idealizado na questao 9  com os resultados obtidos aqui
+    //foi possível perceber que char continua ocupando 1 byte, o int agora é 4 bytes
+    //o float também é 4 bytes, e o double é 8 bytes
+
+    return 0;
+}
