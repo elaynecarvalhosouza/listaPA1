555122dcc3e38f95bf01b73fa19c036529520da4
elaynecarvalhosouza <elayne.carvalho.016@ufrn.edu.br>
2021-06-25 11:13:23 -0300

    CreatedQ8

--- questao8/q8.c
+++ questao8/q8.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int main(){
-int vet[] = {4,9,13};
-int i;
-for(i=0;i<3;i++){
-printf("%d ",*(vet+i));
-}
-}
+int main()
+{
+    int vet[] = {4, 9, 13};
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        printf("%d ", *(vet + i));
+    }
+}//Imprime os endereços das posições do vetor vet.
--- questao8/q8b.c
+++ questao8/q8b.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int main(){
-int vet[] = {4,9,13};
-int i;
-for(i=0;i<3;i++){
-printf("%X ",vet+i);
-}
-}
+int main()
+{
+    int vet[] = {4, 9, 13};
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        printf("%X ", vet + i);
+    }
+}//Imprime os valores do vetor vet
