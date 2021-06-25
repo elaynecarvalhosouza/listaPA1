5e538e97bd574716c30b07998e6fa6e46d9d7544
elaynecarvalhosouza <elayne.carvalho.016@ufrn.edu.br>
2021-06-25 11:09:33 -0300

    createdQ7

--- questao7/q7.c
+++ questao7/q7.c
@@ -4,8 +4,8 @@ int main()
 {
     int mat[4], *p, x;
 
-    printf("%d\n",p = mat + 1);
-    //printf("%d",p = mat++);
-    //printf("%d",p = ++mat);
-    printf("%d",x = (*mat)++);
+    printf("%d\n", p = mat + 1); //é válida porque é um ponteiro recebendo um endereço de memória do mesmo tipo dele.
+    printf("%d", p = mat++);     //Não é válido porque não é permitido o incremento de endereço de vetores.
+    printf("%d", p = ++mat);     //Não é válido porque não é permitido o incremento de endereço de vetores.
+    printf("%d", x = (*mat)++);  //é válida porque uma variável inteira x recebendo o valor que está na primeira posição do vetor "mat", e depois o mesmo é incrementado.
 }
