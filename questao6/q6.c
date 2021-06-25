8d9ea30567992add61a07957c7e4e8a5958a6cba
elaynecarvalhosouza <elayne.carvalho.016@ufrn.edu.br>
2021-06-25 11:02:55 -0300

    createdQ6

--- questao6/q6.c
+++ questao6/q6.c
@@ -3,13 +3,12 @@ int main()
     int pulo[10];
     int *px, i;
     px = pulo;
-    for (i=0; i<10; i++){
-       //*(px+i) = i+1; //x[i] = i+1;
-       px[i] = i+1; //maneira mais simples
+    for (i = 0; i < 10; i++)
+    {
+        px[i] = i + 1;
     }
-
     printf("*px = %d \n ", *(pulo + 2));
-   
+    //A expressão *(pulo + 2); referencia o valor do terceiro elemento do vetor.
     return 0;
 
-}
+} //Fim código
