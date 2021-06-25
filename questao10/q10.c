1a16e7657f91ffe0e62b81077a3dd24b6dc2342c
elaynecarvalhosouza <elayne.carvalho.016@ufrn.edu.br>
2021-06-25 11:27:57 -0300

    createdQ10

--- questao10/q10.c
+++ questao10/q10.c
@@ -2,44 +2,40 @@
 
 int main()
 {
-    //para roda o x com o TIPO que preferi descomente;
 
-    
     char x[4];
-    printf("endereço inicial x= %d \n",x);
-    printf("x = %d \n", x+1); // somou +1 (1*1)
-    printf("x = %d \n", x+2); // somou +2 (2*1)
-    printf("x = %d \n", x+3); // somou +3 (3*1)
-    
+    printf("endereço inicial x= %d \n", x);
+    printf("x = %d \n", x + 1); // somou +1 
+    printf("x = %d \n", x + 2); // somou +2 
+    printf("x = %d \n", x + 3); // somou +3 
 
     /*
     int x[4];
     printf("endereço inicial x= %d \n",x);
-    printf("x = %d \n", x+1); // somou +4 (1*4)
-    printf("x = %d \n", x+2); // somou +8 (2*4)
-    printf("x = %d \n", x+3); // somou +12 (3*4)
+    printf("x = %d \n", x+1); // somou +4 
+    printf("x = %d \n", x+2); // somou +8 
+    printf("x = %d \n", x+3); // somou +12 
     */
 
     /*
     float x[4];
     printf("endereço inicial x= %d \n",x);
-    printf("x = %d \n", x+1); // somou +4 (1*4)
-    printf("x = %d \n", x+2); // somou +8 (2*4)
-    printf("x = %d \n", x+3); // somou +12 (3*4)
+    printf("x = %d \n", x+1); // somou +4 
+    printf("x = %d \n", x+2); // somou +8 
+    printf("x = %d \n", x+3); // somou +12 
     */
 
     /*
     double x[4];
     printf("endereço inicial x= %d \n",x);
-    printf("x = %d \n", x+1); // somou +8 (1*8)
-    printf("x = %d \n", x+2); // somou +16 (2*8)
-    printf("x = %d \n", x+3); // somou +24 (3*8)
+    printf("x = %d \n", x+1); // somou +8 
+    printf("x = %d \n", x+2); // somou +16 
+    printf("x = %d \n", x+3); // somou +24 
     */
 
-
-    //comparando com o que foi idealizado na questao 9  com os resultados obtidos aqui
-    //foi possível perceber que char continua ocupando 1 byte, o int agora é 4 bytes
-    //o float também é 4 bytes, e o double é 8 bytes
-
+    /*comparando com o que foi idealizado na questao 9 com os resultados obtidos nesta questão
+    foi possível perceber que char continua ocupando 1 byte, o int agora ocupa 4 bytes e
+    o float também é 4 bytes, porfim o double ocupa 8 bytes.
+    */
     return 0;
 }
