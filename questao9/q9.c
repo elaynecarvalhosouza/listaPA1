3ac40dfdfb0f96e6a577040febe52c3a24748109
elaynecarvalhosouza <elayne.carvalho.016@ufrn.edu.br>
2021-06-25 11:18:22 -0300

    CreatedQ9

--- questao9/q9.c
+++ questao9/q9.c
@@ -3,28 +3,28 @@
 int main()
 {
 
-    // levando em concideração char ocupa 1 byte, int 2 bytes, float 4 byts, e double 8 bytes
-    // e que o o endereço de memoria de x[0] é 4092
+    /* Sabendo que char ocupa 1 byte, int 2 bytes, float 4 byts, e double 8 bytes
+    e que o endereço de memoria de x[0] é 4092 como falado na questão*/
 
     char x[4];
-    printf("x = %d \n", x+1); // saida de 4093
-    printf("x = %d \n", x+2); // saida de 4094
-    printf("x = %d \n", x+3); // saida de 4095
+    printf("x = %d \n", x + 1); // a saída foi 4093
+    printf("x = %d \n", x + 2); // a saída foi 4094
+    printf("x = %d \n", x + 3); // a saída foi 4095
 
     int x[4];
-    printf("x = %d \n", x+1); // saida de 4094
-    printf("x = %d \n", x+2); // saida de 4096
-    printf("x = %d \n", x+3); // saida de 4098
+    printf("x = %d \n", x + 1); // a saída foi 4094
+    printf("x = %d \n", x + 2); // a saída foi 4096
+    printf("x = %d \n", x + 3); // a saída foi 4098
 
     float x[4];
-    printf("x = %d \n", x+1); // saida de 4096
-    printf("x = %d \n", x+2); // saida de 4100
-    printf("x = %d \n", x+3); // saida de 4104
+    printf("x = %d \n", x + 1); // a saída foi 4096
+    printf("x = %d \n", x + 2); // a saída foi 4100
+    printf("x = %d \n", x + 3); // a saída foi 4104
 
     double x[4];
-    printf("x = %d \n", x+1); // saida de 4100
-    printf("x = %d \n", x+2); // saida de 4108
-    printf("x = %d \n", x+3); // saida de 4116
+    printf("x = %d \n", x + 1); // a saída foi 4100
+    printf("x = %d \n", x + 2); // a saída foi 4108
+    printf("x = %d \n", x + 3); // a saída foi 4116
 
     return 0;
-}
+}//Fim código
