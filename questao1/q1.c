5393dd76cac78b592c265b49aaa65e2bcfa46f75
elaynecarvalhosouza <elayne.carvalho.016@ufrn.edu.br>
2021-06-25 10:11:08 -0300

    createq1

--- questao1/q1.c
+++ questao1/q1.c
@@ -7,10 +7,10 @@ int main(){
     p = &i;
     q = &j;
 
-    printf("A expressão 'p == &i' retorna o valor %d\n", (p== &i));
-    printf("A expressão '*p - *q' retorna o valor %d\n", (*p - *q));
-    printf("A expressão '**&p' retorna o valor %d\n", (**&p));
-    printf("A expressão '3 - *p/(*q) + 7' retorna o valor %d\n", (3 - *p/(*q) + 7));
+    printf("A expressão 'p == &i' retorna o valor %d\n", (p== &i)); //retorna o valor 1.
+    printf("A expressão '*p - *q' retorna o valor %d\n", (*p - *q)); //retorna o valor -2.
+    printf("A expressão '**&p' retorna o valor %d\n", (**&p)); //retorna o valor 3.
+    printf("A expressão '3 - *p/(*q) + 7' retorna o valor %d\n", (3 - *p/(*q) + 7)); //retorna o valor 10.
 
     return 0;
-}
+}//Fim código
